baa670b6af1f06aea25e9c298c28a575c939d5ab
Leo Kuang <leo.kuang@xero.com>
2022-10-10 18:27:27 +1100

    Finish Q70

---
+++ Leetcode069/Leetcode069.cs
@@ -0,0 +1,57 @@
+﻿/*
+   69. Sqrt(x) 
+
+   Given a non-negative integer x, compute and return the square root of x.
+   
+   Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+   
+   Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+   
+   
+   
+   Example 1:
+   
+   Input: x = 4
+   Output: 2
+   Example 2:
+   
+   Input: x = 8
+   Output: 2
+   Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+
+ */
+
+
+namespace Leetcode069
+{
+    internal class Leetcode069
+    {
+        static void Main(string[] args)
+        {
+            var result = MySqrt(8);
+        }
+
+        public static int MySqrt(int x) 
+        {
+            int l = 0;
+            int r = x;
+            int ans = -1;
+
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if ((long)mid * mid <= x)
+                {
+                    ans = mid;
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ Leetcode070/Leetcode070.cs
@@ -0,0 +1,49 @@
+﻿/*
+ 70. Climbing Stairs
+
+ You are climbing a staircase. It takes n steps to reach the top.
+   
+   Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+   
+   Example 1:
+   
+   Input: n = 2
+   Output: 2
+   Explanation: There are two ways to climb to the top.
+   1. 1 step + 1 step
+   2. 2 steps
+
+   Example 2:
+   
+   Input: n = 3
+   Output: 3
+   Explanation: There are three ways to climb to the top.
+   1. 1 step + 1 step + 1 step
+   2. 1 step + 2 steps
+   3. 2 steps + 1 step
+ */
+
+namespace Leetcode070
+{
+    internal class Leetcode070
+    {
+        static void Main(string[] args)
+        {
+            var result = ClimbStairs(4);
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            int p = 0, q = 0, r = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                p = q;
+                q = r;
+                r = p + q;
+            }
+
+            return r;
+        }
+    }
+}
