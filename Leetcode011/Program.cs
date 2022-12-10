0e498e8ae42c5489cf6cea760ab0687441a9e040
Leo Kuang <leo.kuang@xero.com>
2022-12-10 21:58:58 +1100

    Finish Q011

---
+++ Leetcode011/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ *11. Container With Most Water
+   You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+   
+   Find two lines that together with the x-axis form a container, such that the container contains the most water.
+   
+   Return the maximum amount of water a container can store.
+   
+   Notice that you may not slant the container.
+   
+   Example 1:
+   
+   Input: height = [1,8,6,2,5,4,8,3,7]
+   Output: 49
+   Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+   
+   Example 2:
+   
+   Input: height = [1,1]
+   Output: 1
+   
+   
+   Constraints:
+   
+   n == height.length
+   2 <= n <= 105
+   0 <= height[i] <= 104
+ */
+namespace Leetcode011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int MaxArea(int[] height)
+        {
+            var l = 0;
+            var r = height.Length - 1;
+
+            var ans = 0;
+
+            while (l < r)
+            {
+                var area = Math.Min(height[l], height[r]) * (r - l);
+
+                ans = Math.Max(ans, area);
+
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
