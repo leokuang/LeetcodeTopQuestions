fd16292b3f91d730cc6366b77284aaf33796a6cd
Leo <leokuang4@gmail.com>
2022-12-10 23:14:21 +1100

    Finish Q015

---
+++ Leetcode015/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * 15. 3Sum
+   Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+   
+   Notice that the solution set must not contain duplicate triplets.
+   
+   Example 1:
+   
+   Input: nums = [-1,0,1,2,-1,-4]
+   Output: [[-1,-1,2],[-1,0,1]]
+   Explanation: 
+   nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+   nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+   nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+   The distinct triplets are [-1,0,1] and [-1,-1,2].
+   Notice that the order of the output and the order of the triplets does not matter.
+   
+   Example 2:
+   
+   Input: nums = [0,1,1]
+   Output: []
+   Explanation: The only possible triplet does not sum up to 0.
+   
+   Example 3:
+   
+   Input: nums = [0,0,0]
+   Output: [[0,0,0]]
+   Explanation: The only possible triplet sums up to 0.
+   
+   
+   Constraints:
+   
+   3 <= nums.length <= 3000
+   -10^5 <= nums[i] <= 10^5
+ * 
+ */
+namespace Leetcode015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+        }
+
+        public static IList<IList<int>> ThreeSum(int[] nums) 
+        {
+            Array.Sort(nums);
+            var ans = new List<IList<int>>();
+            
+            for (var k = 0; k < nums.Length - 2; k++)
+            {
+
+                if (nums[k] > 0)
+                {
+                    break;
+                }
+
+                if (k > 0 && nums[k] == nums[k - 1])
+                {
+                    continue;
+                }
+
+                var l = k + 1;
+                var r = nums.Length - 1;
+
+                while (l < r)
+                {
+                    var sum = nums[k] + nums[l] + nums[r];
+
+                    if (sum < 0)
+                    {
+                        while (l < r && nums[l] == nums[++l]);
+                    }
+                    else if (sum > 0)
+                    {
+                        while (l < r && nums[r] == nums[--r]);
+                    }
+                    else
+                    {
+                        ans.Add(new List<int>(){nums[k], nums[l], nums[r]});
+                        while (l < r && nums[l] == nums[++l]) ;
+                        while (l < r && nums[r] == nums[--r]) ;
+                    }
+                }
+            }
+
+            return ans;
+            
+        }
+    }
+}
