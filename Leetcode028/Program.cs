e0575e53252e847d2a0020ec835f76060e25143d
Leo Kuang <leo.kuang@xero.com>
2022-12-15 10:37:35 +1100

    Q028

--- Leetcode028/Program.cs
+++ Leetcode028/Program.cs
@@ -20,6 +20,9 @@
    1 <= haystack.length, needle.length <= 10^4
    haystack and needle consist of only lowercase English characters.
  */
+
+using System.Globalization;
+
 namespace Leetcode028
 {
     internal class Program
@@ -29,10 +32,75 @@ namespace Leetcode028
             Console.WriteLine("Hello, World!");
         }
 
-        public int StrStr(string haystack, string needle) 
+        public static int StrStr(string haystack, string needle) 
         {
+            if (haystack.Length < needle.Length)
+            {
+                return -1;
+            }
+            
+            int i = 0;
+            int j = 0;
+            var next = new int[needle.Length];
 
+            ComputeNextArray(needle, next);
+
+            while (i < haystack.Length)
+            {
+                if (haystack[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                }
+
+                if (j == needle.Length)
+                {
+                    return i - j;
+                }
+                else if (i < haystack.Length && haystack[i] != needle[j])
+                {
+                    if (j > 0)
+                    {
+                        j = next[j - 1];
+                    }
+                    else // j == 0
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            return -1;
         }
 
+        public static void ComputeNextArray(string needle, int[] next)
+        {
+            var len = 0;
+            var i = 1;
+
+            next[0] = 0;
+
+            while (i < needle.Length)
+            {
+                if (needle[len] == needle[i])
+                {
+                    len++;
+                    next[i] = len;
+                    i++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        len = next[len - 1];
+                    }
+                    else
+                    {
+                        next[i] = len; // len is 0
+                        i++;
+                    }
+                }
+            }
+        }
     }
 }
